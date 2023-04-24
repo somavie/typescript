48de2d508f3ccef8ab9bf3eaa3197ad25049007c
somavie <somavie1@gmail.com>
2023-04-24 19:14:57 +0100

    Update vetor.ts

--- modulo02/array/vetor.ts
+++ modulo02/array/vetor.ts
@@ -7,3 +7,22 @@ for(i in vegetais){
     console.log(vegetais[i]);
 
 }
+
+let v:string[]=[''];
+
+i=0;
+let a:boolean=true
+while (a) {
+    
+    v[i]="a"+i
+
+
+    i++
+if(i===5){
+
+    a=false;
+}    
+
+}
+
+console.log(v)
